75cf54709334f183a8886fa7847adce4b210fb3a
IamLikhon <likhonadnan55@gmail.com>
2023-06-03 21:38:19 +0600

    june2

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersCollection=client.db('bistroDb').collection('users');
     const menuCollection=client.db('bistroDb').collection('menu');
